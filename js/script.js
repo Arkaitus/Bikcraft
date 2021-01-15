75810618bc77fe78cbe33e1f2b82a68e91518dee
Braian <braian.codex@gmail.com>
2021-01-15 14:27:37 -0300

    Adicionado os arquivos do projeto Bikcraft

---
+++ js/script.js
@@ -0,0 +1,28 @@
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
+if (window.SimpleAnime) {
+
+	new SimpleForm({
+		form: ".formphp",
+		button: "#enviar",
+		erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o e-mail contato@bikcraft.com.</p></div>",
+		sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso!</h2><p>Em breve eu entro em contato com você.</p></div>" 
+});
+
+}
